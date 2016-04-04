5f1a7558d6d64715528dcdcbc927a78aa6de594c
dmattia <dmattia@nd.edu>
2016-04-04 19:05:47 -0400

    Added basic tilt motion support that pushes to database

---
+++ ros-controller-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ros-controller-Bridging-Header.h
+//  ros-controller
+//
+//  Created by David Mattia on 4/1/16.
+//  Copyright © 2016 David Mattia. All rights reserved.
+//
+
+#ifndef ros_controller_Bridging_Header_h
+#define ros_controller_Bridging_Header_h
+
+#import <Firebase/Firebase.h>
+
+#endif /* ros_controller_Bridging_Header_h */
--- ros-controller/ViewController.swift
+++ ros-controller/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import CoreMotion
+import Firebase
+
+public extension Double {
+    func roundToDecimals(decimals: Int = 2) -> Double {
+        let multiplier = Double(10^decimals)
+        return round(multiplier * self) / multiplier
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var activeSwitch: UISwitch!
+    
+    let motionManager : CMMotionManager! = CMMotionManager()
+    let motionRef = Firebase(url:"https://ros.firebaseio.com/speed")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if motionManager.accelerometerAvailable{
+            self.textLabel.transform = CGAffineTransformMakeRotation(CGFloat(M_PI / 2.0))
+            
+            let queue = NSOperationQueue()
+            motionManager.startDeviceMotionUpdatesToQueue(queue, withHandler: { (data, error) in
+                let pitch = data!.attitude.pitch
+                let roll = data!.attitude.roll
+                
+                let motion = [
+                    "linear": (roll + M_PI_2).roundToDecimals(2),
+                    "angular": pitch.roundToDecimals(2)
+                ]
+                if self.activeSwitch.on {
+                    self.motionRef.setValue(motion)
+                }
+            })
+        } else {
+            self.textLabel.text = "Accelerometer data is not available."
+            print("Accelerometer is not available")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
